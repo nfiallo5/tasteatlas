c564fc5f443b0c2810b0caafdd4bda0fdf23a330
Allan <aavendan@gmail.com>
2025-07-30 13:50:29 -0500

    Update DishTable.tsx

--- src/components/DishTable.tsx
+++ src/components/DishTable.tsx
@@ -8,7 +8,7 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
-import Dish from '../interface/Dish';
+import { type Dish } from '../interface/Dish';
 
 export default function DishTable( { data } : { data: Dish[] } ) {
 
